06b108a40304b221b2d05b933801ff0d21a63117
Ciprian Dorin, Craciun <ciprian@volution.ro>
2012-05-16 19:11:02 +0300

    Update (and split) `prepare` script

---
+++ scripts/prepare-all.bash
@@ -0,0 +1,35 @@
+#!/dev/null
+
+if ! test "${#}" -eq 0 ; then
+	echo "[ee] invalid arguments; aborting!" >&2
+	exit 1
+fi
+
+env "${_scripts_env[@]}" "${_scripts}/prepare-distribution"
+
+env "${_scripts_env[@]}" "${_repositories}/mosaic-erlang-tools/scripts/prepare"
+
+env "${_scripts_env[@]}" "${_repositories}/mosaic-node/scripts/prepare"
+
+env "${_scripts_env[@]}" "${_repositories}/mosaic-node-wui/scripts/prepare"
+
+env "${_scripts_env[@]}" "${_repositories}/mosaic-components-rabbitmq/scripts/prepare"
+env "${_scripts_env[@]}" "${_repositories}/mosaic-components-riak-kv/scripts/prepare"
+env "${_scripts_env[@]}" "${_repositories}/mosaic-components-httpg/scripts/prepare"
+
+env "${_scripts_env[@]}" "${_repositories}/mosaic-erlang-drivers/scripts/prepare"
+
+if test "${_mosaic_do_all_java:-${_mosaic_do_all:-true}}" == true ; then
+	env "${_scripts_env[@]}" "${_repositories}/mosaic-java-platform/components-container/scripts/prepare"
+	env "${_scripts_env[@]}" "${_repositories}/mosaic-java-platform/cloudlets/scripts/prepare"
+	env "${_scripts_env[@]}" "${_repositories}/mosaic-java-platform/drivers-stubs/amqp/scripts/prepare"
+	env "${_scripts_env[@]}" "${_repositories}/mosaic-java-platform/drivers-stubs/riak/scripts/prepare"
+fi
+
+if test "${_mosaic_do_all_examples:-${_mosaic_do_all:-true}}" == true ; then
+	env "${_scripts_env[@]}" "${_repositories}/mosaic-examples-realtime-feeds/backend/scripts/prepare"
+	env "${_scripts_env[@]}" "${_repositories}/mosaic-examples-realtime-feeds/frontend/scripts/prepare"
+	env "${_scripts_env[@]}" "${_repositories}/mosaic-java-platform/examples/realtime-feeds-indexer/scripts/prepare"
+fi
+
+exit 0
