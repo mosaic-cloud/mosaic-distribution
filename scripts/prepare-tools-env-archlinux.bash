53321dfa538885a528f5f24b8d8dd714416dece1
Ciprian Dorin Craciun <ciprian@volution.ro>
2012-10-11 21:16:27 +0300

    Update ArchLinux tools environment

--- scripts/prepare-tools-env-archlinux.bash
+++ scripts/prepare-tools-env-archlinux.bash
@@ -1,5 +1,19 @@
 #!/dev/null
 
 _distribution_archlinux_dependencies=(
-		...
+	# mOSAIC custom packages
+	# Archlinux miscellaneous packages
+		python2
+		git
+		zip
+	# Archlinux development packages
+		gcc
+		binutils
+		libtool
+		autoconf
+		automake
+		make
+		pkg-config
+		jansson
+		libxml2
 )
