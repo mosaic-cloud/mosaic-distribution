c8a822dbdddaddd163e4449ffe4ee1034907912d
Ciprian Dorin Craciun <ciprian@volution.ro>
2012-10-11 22:30:38 +0300

    Update clean script

--- scripts/.clean.bash
+++ scripts/.clean.bash
@@ -5,9 +5,29 @@ if ! test "${#}" -eq 0 ; then
 	exit 1
 fi
 
+echo "[ii] cleaning outputs and temporary..." >&2
+
+if test -e "${_outputs}" ; then
+	_outputs="$( readlink -e -- "${_outputs}" )"
+	chmod -R o+w -- "${_outputs}"
+	rm -Rf -- "${_outputs}"
+fi
+if test -L "${_outputs}" ; then
+	rm -- "${_outputs}"
+fi
+
 if test -e "${_temporary}" ; then
+	_temporary="$( readlink -e -- "${_temporary}" )"
 	chmod -R o+w -- "${_temporary}"
 	rm -Rf -- "${_temporary}"
 fi
+if test -L "${_temporary}" ; then
+	rm -- "${_temporary}"
+fi
+
+echo "[ii] cleaning repositories..." >&2
+
+"${_git_bin}" submodule update --quiet --init --recursive
+"${_git_bin}" submodule foreach --quiet --recursive 'chmod -R +w . && git reset -q --hard HEAD && git clean -q -f -d -x'
 
 exit 0
