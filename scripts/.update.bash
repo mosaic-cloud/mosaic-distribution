ffd46c3e58d167bf78c0018a72dbc77c3f64e403
Ciprian Dorin Craciun <ciprian@volution.ro>
2013-01-22 14:46:01 +0200

    Rename zeromq repository

--- scripts/.update.bash
+++ scripts/.update.bash
@@ -10,16 +10,21 @@ fi
 	path="$( readlink -e "${name}" )"
 	name="$( basename "${name}" )"
 	orig_path="$( readlink -e ../mosaic-repositories )/${name}"
-	if test ! -e "${orig_path}" ; then continue ; fi
+	if test ! -e "${orig_path}" ; then
+		echo "[ww] ?? ${name}" >&2
+		continue
+	fi
 	origin_sha1="$( cd "${orig_path}" ; exec git rev-parse HEAD ; )"
 	if test "${sha1}" != "${origin_sha1}" ; then
-		echo "# ${name} -> ${origin_sha1}"
+		echo "[ww] ## ${name} -> ${origin_sha1}" >&2
 		( cd "${path}" ; exec git fetch ; )
 		if ( cd "${path}" ; exec git cat-file -e "${origin_sha1}" ; ) ; then
 			( cd "${path}" ; exec git checkout "${origin_sha1}" ; )
 		else
-			echo "#### !!!!" >&2
+			echo "[ee] #### !!!!" >&2
 		fi
+	else
+		echo "[ii] -- ${name}" >&2
 	fi
 done
 
