22bf6bb0219708b175eb05a615e8d762d4a15f6a
Ciprian Dorin Craciun <ciprian@volution.ro>
2013-07-22 11:03:29 +0300

    Add scripts to run an individual component

---
+++ scripts/run-platform-component.bash
@@ -0,0 +1,34 @@
+#!/dev/null
+
+if ! test "${#}" -eq 1 ; then
+	echo "[ee] invalid arguments; aborting!" >&2
+	exit 1
+fi
+
+_scripts_env=(
+	PATH="$(
+			find "${_repositories}" \
+					-xtype d \
+					-name '.outputs' \
+					-exec test -e {}/package/bin \; \
+					-printf "%p/package/bin:"
+	)${_PATH}"
+	HOME="${_tools}/home"
+	TMPDIR="${_temporary}"
+)
+
+case "${1}" in
+	( riak-kv )
+		_command=( mosaic-components-riak-kv--run-component )
+	;;
+	( rabbitmq )
+		_command=( mosaic-components-rabbitmq--run-component )
+	;;
+	( * )
+		exit 1
+	;;
+esac
+
+exec env -i "${_scripts_env[@]}" "${_command[@]}"
+
+exit 1
