d51b71d406c0763be2b74ac65d3945fc7b09902f
Ciprian Dorin Craciun <ciprian@volution.ro>
2013-11-11 17:53:13 +0200

    Update scripts

--- scripts/.magic.bash
+++ scripts/.magic.bash
@@ -1,28 +1,42 @@
 #!/dev/null
 
-if ! test "${#}" -eq 0 ; then
-	echo "[ee] invalid arguments; aborting!" >&2
-	exit 1
-fi
 
-echo "[ii] preparing..." >&2
-_script_exec "${_scripts}/prepare-all"
-echo "[--]" >&2
-
-echo "[ii] compiling..." >&2
-_script_exec "${_scripts}/compile-all"
-echo "[--]" >&2
-
-echo "[ii] packaging..." >&2
-_script_exec "${_scripts}/package-all"
-echo "[--]" >&2
-
-echo "[ii] deploying..." >&2
-if test "${_mosaic_deploy_skip:-true}" != true ; then
-	_script_exec "${_scripts}/deploy-all"
+if test "${#}" -eq 0 ; then
+	_scripts_roots=( "${_scripts}" )
+	_scripts_suffix='-all'
 else
-	echo "[ww]   -- skipped!" >&2
+	_scripts_roots=()
+	_scripts_suffix=''
+	for _repository in "${@}" ; do
+		_scripts_root="${_mosaic_repositories}/${_repository}/scripts"
+		test -e "${_scripts_root}"
+		_scripts_roots+=( "${_scripts_root}" )
+	done
 fi
-echo "[--]" >&2
+
+
+for _scripts_root in "${_scripts_roots[@]}" ; do
+	
+	echo "[ii] preparing..." >&2
+	_script_exec "${_scripts_root}/prepare${_scripts_suffix}"
+	echo "[--]" >&2
+	
+	echo "[ii] compiling..." >&2
+	_script_exec "${_scripts_root}/compile${_scripts_suffix}"
+	echo "[--]" >&2
+	
+	echo "[ii] packaging..." >&2
+	_script_exec "${_scripts_root}/package${_scripts_suffix}"
+	echo "[--]" >&2
+	
+	echo "[ii] deploying..." >&2
+	if test "${_mosaic_deploy_skip:-true}" != true ; then
+		_script_exec "${_scripts_root}/deploy${_scripts_suffix}"
+	else
+		echo "[ww]   -- skipped!" >&2
+	fi
+	echo "[--]" >&2
+	
+done
 
 exit 0
