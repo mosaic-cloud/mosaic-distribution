37ba0a5f65edae504675759eb1c52cb7b567519b
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-09-11 16:09:36 +0300

    Update repositories

--- .scripts/.advance.bash
+++ .scripts/.advance.bash
@@ -19,7 +19,7 @@ env -i "${_git_env[@]}" "${_git_bin}" \
 			orig_path="$( readlink -e ../mosaic-distribution-dependencies )"
 		;;
 		( * )
-			false
+			continue
 		;;
 	esac
 	if test "${orig_path}" == "${path}" ; then
