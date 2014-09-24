f0ce23741b53cf4c89f097a1de227392a9b4a350
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-09-24 20:16:16 +0300

    Fix JZMQ build script

--- mosaic-scripts/prepare-jzmq.bash
+++ mosaic-scripts/prepare-jzmq.bash
@@ -29,9 +29,11 @@ chmod -R a=rX,u=rwX -- "${_outputs}"
 cd -- "${_outputs}"
 
 _CFLAGS="-I${pallur_pkg_java}/include ${pallur_CFLAGS}"
+_CXXFLAGS="-I${pallur_pkg_java}/include ${pallur_CXXFLAGS}"
 _LDFLAGS="-L${pallur_pkg_java}/lib ${pallur_LDFLAGS}"
 _LIBS="${pallur_LIBS}"
 _JAVA_HOME="${pallur_pkg_java}"
+_PATH="${_JAVA_HOME}/bin:${_PATH}"
 
 echo "[ii] building..." >&2
 
@@ -40,35 +42,41 @@ _do_exec \
 
 _do_exec env \
 			CFLAGS="${_CFLAGS}" \
+			CXXFLAGS="${_CXXFLAGS}" \
 			LDFLAGS="${_LDFLAGS}" \
 			LIBS="${_LIBS}" \
 			JAVA_HOME="${_JAVA_HOME}" \
+			PATH="${_PATH}" \
 	./configure \
 		--prefix="${_tools}/pkg/jzmq" \
 		--with-zeromq="${pallur_pkg_zeromq}"
 
 _do_exec env \
 			CFLAGS="${_CFLAGS}" \
+			CXXFLAGS="${_CXXFLAGS}" \
 			LDFLAGS="${_LDFLAGS}" \
 			LIBS="${_LIBS}" \
 			JAVA_HOME="${_JAVA_HOME}" \
+			PATH="${_PATH}" \
 	make -j 1
 
 _do_exec \
 	gcc -shared -o ./libjzmq.so \
 		-I "${_JAVA_HOME}/include" \
+		-I "${_JAVA_HOME}/include/linux" \
 		-L "${_JAVA_HOME}/lib" \
 		-I "${pallur_pkg_zeromq}/include" \
 		-L "${pallur_pkg_zeromq}/lib" \
 		-I ./src \
 		-w \
-		${CFLAGS:-} ${LDFLAGS:-} \
+		${_CXXFLAGS:-} \
 		./src/ZMQ.cpp \
 		./src/Context.cpp \
 		./src/Socket.cpp \
 		./src/Poller.cpp \
 		./src/util.cpp \
-		${pallur_LIBS:-} \
+		${_LDFLAGS:-} \
+		${_LIBS:-} \
 		-Wl,-Bstatic -lzmq -luuid -Wl,-Bdynamic \
 		-Wl,-Bstatic -lstdc++ -Wl,-Bdynamic \
 		-static-libgcc -static-libstdc++
