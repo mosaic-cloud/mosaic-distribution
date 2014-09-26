793d77b384eed2946793cdbc698aed1d45d391fd
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-09-26 13:28:27 +0300

    Update Git submodules advance script

--- .scripts/.advance.bash
+++ .scripts/.advance.bash
@@ -5,6 +5,12 @@ if ! test "${#}" -eq 0 ; then
 	exit 1
 fi
 
+if test "${_advance_auto_init:-false}" == true ; then
+	echo "[ww] initializing submodules..." >&2
+	env -i "${_git_env[@]}" "${_git_bin}" \
+			submodule update --quiet --init .
+fi
+
 env -i "${_git_env[@]}" "${_git_bin}" \
 		submodule foreach --quiet \
 			'echo "$( pwd ) ${name} ${sha1}"' \
@@ -44,16 +50,21 @@ env -i "${_git_env[@]}" "${_git_bin}" \
 				exec env -i "${_git_env[@]}" "${_git_bin}" cat-file -e "${origin_sha1}"
 		) ; then (
 				cd "${path}"
-				env -i "${_git_env[@]}" "${_git_bin}" \
-						checkout "${origin_sha1}"
-				env -i "${_git_env[@]}" "${_git_bin}" \
-						submodule update --quiet --recursive --init --force
+				env -i "${_git_env[@]}" "${_git_bin}" checkout "${origin_sha1}"
+				env -i "${_git_env[@]}" "${_git_bin}" submodule update --quiet --recursive --init --force
 		) ; else
 			echo "[ee] #### !!!!" >&2
 		fi
+		env -i "${_git_env[@]}" "${_git_bin}" add "${path}"
 	else
 		echo "[ii] -- ${name}" >&2
 	fi
 done
+
+if test "${_advance_auto_init:-false}" == true ; then
+	echo "[ww] deinitializing submodules..." >&2
+	env -i "${_git_env[@]}" "${_git_bin}" \
+			submodule deinit --quiet .
+fi
 
 exit 0
