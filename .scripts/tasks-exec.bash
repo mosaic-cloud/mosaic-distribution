60a5f89503e994d15d5a44a8f13924780ddb10a8
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-10-01 16:29:39 +0300

    Update make invocation (allow job parallelism)

--- .scripts/tasks-exec.bash
+++ .scripts/tasks-exec.bash
@@ -5,8 +5,25 @@ if ! test "${#}" -ge 1 ; then
 	exit 1
 fi
 
-_do_exec1 make -f <(
-		cat <<'EOS'
+_make_jobs=4
+_make_load=8
+
+_make_arguments=(
+	--directory="${_temporary}"
+	--jobs="${_make_jobs}"
+	--load-average="${_make_load}"
+	--keep-going
+	--no-builtin-rules
+	--no-builtin-variables
+	--warn-undefined-variables
+	--silent
+	--no-print-directory
+	--output-sync=target
+)
+
+_do_exec1 make \
+		"${_make_arguments[@]}" \
+		-f <( cat <<'EOS'
 .DEFAULT : default
 .PHONY :
 .SUFFIXES :
@@ -23,8 +40,6 @@ EOS
 			-e '/^\t/! b' \
 			-e 'w /dev/stderr' -e 'Q 1'
 ) \
-	-s -R -r -e --warn-undefined-variables \
-	-j 1 \
 	-- "${@}"
 
 exit 0
