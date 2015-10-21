be7713d8327c44807c7b070bfaf256d870296cb6
Ciprian Dorin Craciun <ciprian@volution.ro>
2015-10-21 19:34:30 +0300

    Allow make to be configured through environment

--- .scripts/tasks-exec.bash
+++ .scripts/tasks-exec.bash
@@ -5,21 +5,29 @@ if ! test "${#}" -ge 1 ; then
 	exit 1
 fi
 
-_make_jobs=4
-_make_load=8
+_make_quiet="${pallur_do_scripts_make_quiet:-true}"
+_make_continue="${pallur_do_scripts_make_continue:-true}"
+_make_jobs="${pallur_do_scripts_make_jobs:-4}"
+_make_load="${pallur_do_scripts_make_load:-8}"
 
 _make_arguments=(
 	--directory="/tmp"
 	--jobs="${_make_jobs}"
 	--load-average="${_make_load}"
-	--keep-going
 	--no-builtin-rules
 	--no-builtin-variables
 	--warn-undefined-variables
-	--silent
 	--no-print-directory
-	--output-sync=target
 )
+
+if test "${_make_quiet}" == true ; then
+	_make_arguments+=( --silent --output-sync=target )
+else
+	_make_arguments+=( --output-sync=none )
+fi
+if test "${_make_continue}" == true ; then
+	_make_arguments+=( --keep-going )
+fi
 
 _do_exec1 make \
 		"${_make_arguments[@]}" \
